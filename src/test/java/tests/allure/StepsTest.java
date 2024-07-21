c98e1018d36e3c291f0b0b5bf951ce1011f4e847
Roman <dcba97@inbox.ru>
2024-07-22 00:57:56 +0500

    updated build.gradle

--- src/test/java/tests/allure/StepsTest.java
+++ src/test/java/tests/allure/StepsTest.java
@@ -2,6 +2,7 @@ package tests.allure;
 
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exist;
@@ -11,6 +12,7 @@ import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 import static org.openqa.selenium.By.linkText;
 
+@Tag("remote")
 public class StepsTest {
 
     private static final String REPOSITORY = "eroshenkoam/allure-example";
