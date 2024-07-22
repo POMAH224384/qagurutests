3373f315f12a4700873ad59015ffac7114d47136
Roman <dcba97@inbox.ru>
2024-07-22 16:00:31 +0500

    added StepsTest and tagged as remote

--- src/test/java/tests/allure/AttachmentsTest.java
+++ src/test/java/tests/allure/AttachmentsTest.java
@@ -22,7 +22,7 @@ public class AttachmentsTest {
         Configuration.browser = "chrome";
         Configuration.browserVersion = "104.0";
         Configuration.browserSize = "1920x1080";
-        Configuration.remote = "http://localhost:8080";
+        Configuration.remote = "http://selenoid:4444/wd/hub";
     }
 
     @Test
