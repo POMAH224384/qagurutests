5bbc8315d4d08ade1971f3d1000a14b3d4bf10b4
Roman <dcba97@inbox.ru>
2024-07-24 10:02:35 +0500

    added StepsTest and tagged as remote

--- src/test/java/tests/demoqa/config/PracticeFormConfig.java
+++ src/test/java/tests/demoqa/config/PracticeFormConfig.java
@@ -5,7 +5,7 @@ import org.aeonbits.owner.Config;
 public interface PracticeFormConfig extends Config {
 
     @Key("remoteUrl")
-    @DefaultValue("")
+    @DefaultValue("http://127.0.0.1:4444/wd/hub")
     public String getRemoteUrl();
 
 }
