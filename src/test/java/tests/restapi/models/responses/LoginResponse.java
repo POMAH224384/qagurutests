b2fc12a51fb00be6b6567004bfe4446a1e494f92
Roman <dcba97@inbox.ru>
2024-07-27 23:22:41 +0500

    added a lot of shit

--- src/test/java/tests/restapi/models/responses/LoginResponse.java
+++ src/test/java/tests/restapi/models/responses/LoginResponse.java
@@ -1,8 +1,10 @@
 package tests.restapi.models.responses;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class LoginResponse {
     String token;
     String error;
